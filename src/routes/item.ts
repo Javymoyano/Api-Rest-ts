55825860fbbb199498a71fe1d83af57383b686e8
Javymoyano <javymoyano80@gmail.com>
2023-02-22 13:18:50 -0300

    Actualizacion del proyecto en curso

--- src/routes/item.ts
+++ src/routes/item.ts
@@ -1,10 +1,19 @@
 import { Router } from "express";
+import {
+  deleteItem,
+  getItem,
+  getItems,
+  postItem,
+  updateItem,
+} from "../controllers/item";
 
 const router = Router();
 
 //Primer arguento es la ruta y el segundo argumento es una funcion con el request y el response
-router.get("/", (req, res) => {
-  res.send({ Data: "Aqui van los modelos" });
-});
+router.get("/", getItems);
+router.get("/:id", getItem);
+router.post("/", postItem);
+router.put("/:id", updateItem);
+router.delete("/:id", deleteItem);
 
 export { router };
